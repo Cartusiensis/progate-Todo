e481f4c6e7f6682fc74ffc166856984a3793b583
cartusiensis <170923351+Cartusiensis@users.noreply.github.com>
2024-06-03 14:37:42 +0700

    Develop delete todo feature

--- src/App.jsx
+++ src/App.jsx
@@ -30,12 +30,23 @@ function App() {
     })
 
     setTodos(updateTodos)
-}
+  }
+
+  const deleteTodo = (todoId) => {
+    const deleteTodos = todos.filter((todo) => {
+      return todo.id !== todoId
+    })
+    setTodos(deleteTodos)
+  }
 
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>My Todo List</h1>
-      <Todos todos={todos} toggleCompleted={toggleCompleted} />
+      <Todos 
+        todos={todos} 
+        toggleCompleted={toggleCompleted} 
+        deleteTodo={deleteTodo}
+      />
     </div>
   )
 }
